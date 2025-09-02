2fed8b1f3a825be8d8678f43113a8c1beb0dbd81
Mohammed Alaa <modi20272@gmail.com>
2025-09-02 10:13:49 +0400

    adv imgs

---
+++ script.js
@@ -0,0 +1,37 @@
+let img = document.querySelector(".image img");
+
+let idx = 1;
+
+let myInterval;
+
+function handleLooping() {
+
+    myInterval = setInterval(() => {
+
+        img.style.opacity = 0;
+
+        setTimeout(() => {
+            img.src = `./p${idx}.png`;
+        }, 500)
+
+        setTimeout(() => {
+            img.style.opacity = 1;
+        }, 500)
+        idx = (idx === 5 ? 1 : ++idx);
+    }, 1500);
+}
+
+handleLooping();
+let looping = true;
+
+function handleContinueClick() {
+    if (!looping) {
+        looping = true;
+        handleLooping();
+    }
+}
+
+function handleStopClick() {
+    clearInterval(myInterval);
+    looping = false;
+}
